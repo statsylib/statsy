021e722c9c4be287331ab2428f08dad36df8045f
statsy <statsy@gmail.com>
2024-12-18 20:35:29 -0500

    mouse dragging points

--- src/SingleExplanatoryVariableCorrelationChart.ts
+++ src/SingleExplanatoryVariableCorrelationChart.ts
@@ -1,63 +1,93 @@
-import type { StatisticalDataSet } from './StatisticalDataSet.ts';
-import type { SingleExplanatoryVariableStatisticalDataSet } from './SingleExplanatoryVariableStatisticalDataSet.ts';
+import { StatisticalDataSet } from './StatisticalDataSet.ts';
+import { SingleExplanatoryVariableStatisticalDataSet } from './SingleExplanatoryVariableStatisticalDataSet.ts';
 
-
-/**
- * Draws a two dimensional chart on the given context with the given datapoints and a linear regression line.
- */
-export class SingleExplanatoryVariableCorrelationChart {
+class SingleExplanatoryVariableCorrelationChart {
 
     /**
      * The size of the border on the left and right of the chart where nothing will be drawn except 
      * for the labels.
      */
     xBorder: number = 10;
-
+  
     /**
      * The size of the border on the top and bottom of the chart where nothing will be drawn except 
      * for the labels.
      */
     yBorder: number = 40;
-
+  
     #labelTextHeight: number = 20;
     #xPadding: number = this.xBorder + this.#labelTextHeight;
     #yPadding: number = this.yBorder + this.#labelTextHeight;
-
+  
     textFontSize: number = 20;
 
+    dataPointRadius: number = 5;
+  
     constructor(public context: any, public dataset: StatisticalDataSet) {
         this.context = context;
         this.dataset = dataset;
+  
+        console.log('attempting to add mous event listener on canvas');
+        context.canvas.addEventListener('click', this.onClick.bind(this));
+        context.canvas.addEventListener('mousedown', this.onMouseDown.bind(this));
+    }
+
+    onMouseDown(event: any) {
+        console.log('mouse down');
+    }
+
+    onClick(event: any) {
+        const canvasBoundingRect = this.context.canvas.getBoundingClientRect();
+        const x = event.pageX - canvasBoundingRect.left;
+        const y = event.pageY - canvasBoundingRect.top;
+        const dataX = this.fromScreenWidthUnits(x);
+        const dataY = this.fromScreenHeightUnits(y)
+        this.drawCircle(x, y, this.dataPointRadius / 2, 'yellow');
+        //console.log('adjusted values at (' + x + ',' + y + ')');
+        //console.log('data values at click at (' + dataX + ',' + dataY + ')');
+        if (this.dataset) {
+            for (let i: number = 0; i < this.dataset.data.length; i++) {
+                const distance = Math.sqrt((dataX - this.dataset.data[i][this.dataset.explanatory_value_name]) ** 2 + (dataY - this.dataset.data[i][this.dataset.dependent_variable_name]) ** 2);
+                //console.log('distance: ' + distance);
+                if (distance <= (this.dataPointRadius * 5)) {
+                    console.log('you got a circle at (' + x + ',' + y + ')');
+                }
+            }
+        }
     }
 
     toScreenHeightUnits(value: number): number {
-        return this.context.canvas.height - this.#yPadding - (value * (this.context.canvas.height - (this.#yPadding*2)) / this.dataset.max_dependent_variable_value);
+        const chartHeight = this.context.canvas.height - (this.#yPadding*2);
+        return this.context.canvas.height - this.#yPadding - (value * chartHeight / this.dataset.max_dependent_variable_value);
     }
-
+  
     fromScreenHeightUnits(value: number): number {
-        return value * this.dataset.max_dependent_variable_value / (this.context.canvas.height - (this.#yPadding*2));
+        const chartHeight = this.context.canvas.height - (this.#yPadding*2);
+        return ((this.context.canvas.height - this.#yPadding - value) * this.dataset.max_dependent_variable_value) / chartHeight;
     }
-
+  
     toScreenWidthUnits(value: number): number {
-        return this.#xPadding + (value * (this.context.canvas.width - (this.#xPadding*2)) / this.dataset.max_explanatory_value);
+        const chartWidth = this.context.canvas.width - (this.#xPadding*2);
+        return (value * chartWidth / this.dataset.max_explanatory_value) + this.#xPadding;
     }
-
+  
     fromScreenWidthUnits(value: number): number {
-        return value * this.dataset.max_explanatory_value / (this.context.canvas.width - (this.#xPadding*2));
+        const chartWidth = this.context.canvas.width - (this.#xPadding*2);
+        return ((value - this.#xPadding) * this.dataset.max_explanatory_value) / chartWidth;
     }
-
+  
     drawDataPoints() {
         if (this.dataset) {
             for (let i: number = 0; i < this.dataset.data.length; i++) {
                 const xValue = this.toScreenWidthUnits(this.dataset.data[i][this.dataset.explanatory_value_name]);
                 const yValue = this.toScreenHeightUnits(this.dataset.data[i][this.dataset.dependent_variable_name]);
-                this.drawCircle(xValue, yValue, 5, 'white')
+                this.drawCircle(xValue, yValue, this.dataPointRadius, 'white')
             }
         } else {
             console.log('no data points to draw');
         }
     }
-
+  
     drawAxes() {
         this.drawSolidRectangle(0,0,this.context.canvas.width, this.context.canvas.height, 'black')
         this.drawLine(this.#xPadding, this.context.canvas.height - this.#yPadding, this.context.canvas.width - this.#xPadding, this.context.canvas.height - this.#yPadding, 'white', 2);
@@ -67,7 +97,7 @@ export class SingleExplanatoryVariableCorrelationChart {
             this.drawCenteredHorizontalText(0, this.context.canvas.height, this.context.canvas.width, this.context.canvas.height - this.#yPadding + 1, this.dataset.getExplanatoryValueName(), this.textFontSize, 'white');
         }
     }
-
+  
     drawLeastSquaresFitLine(varX: string, varY: string) {
         const regression = this.dataset.leastSquaresRegression(varX, varY);
         const leastSquaresX1 = this.toScreenWidthUnits(this.dataset.minKeyValue(this.dataset.explanatory_value_name));
@@ -76,7 +106,7 @@ export class SingleExplanatoryVariableCorrelationChart {
         const leastSquaresY2 = this.toScreenHeightUnits(regression.m * this.dataset.max_explanatory_value + regression.b);
         this.drawLine(leastSquaresX1, leastSquaresY1, leastSquaresX2, leastSquaresY2, 'red', 2);
     }
-
+  
     drawCorrelationCoefficient(varX: string, varY: string) {
         if (this.dataset) {
             const coefficient = (this.dataset as SingleExplanatoryVariableStatisticalDataSet).correlationCoefficient(varX, varY);
@@ -84,26 +114,26 @@ export class SingleExplanatoryVariableCorrelationChart {
             this.drawCenteredHorizontalText(50, this.yBorder, 200, 0, print_text, 14, 'white');
         }
     }
-
+  
     drawCircle(x: number, y: number, radius: number, color: string) {
         this.context.beginPath();
         this.context.arc(x, y, radius, 0, Math.PI * 2);  
         this.context.fillStyle = color;  
         this.context.fill();
     }
-
+  
     drawSolidRectangle(x: number, y: number, width: number, height: number, color: string) {
         this.context.beginPath();
         this.context.fillStyle = color;
         this.context.fillRect(x, y, width, height);
     }
-
+  
     drawHollowRectangle(x: number, y: number, width: number, height: number, color: string) {
         this.context.beginPath();
         this.context.strokeStyle = color;
         this.context.strokeRect(x, y, width, height);
     }
-
+  
     drawLine(startX: number, startY: number, endX: number, endY: number, color: string, strokeWidth: number) {
         this.context.lineWidth = strokeWidth;
         this.context.strokeStyle = color;
@@ -112,7 +142,7 @@ export class SingleExplanatoryVariableCorrelationChart {
         this.context.lineTo(endX, endY);
         this.context.stroke();
     }
-
+  
     drawCenteredHorizontalText(x1: number, y1: number, x2: number, y2: number, text: string, fontSize: number, color: string) {
         const measurement = this.context.measureText(text);
         const textWidth = measurement.width;
@@ -123,7 +153,7 @@ export class SingleExplanatoryVariableCorrelationChart {
         this.context.font = fontSize + "px arial";
         this.context.fillText(text, textX, textY);
     }
-
+  
     drawCenteredVerticalText(x1: number, y1: number, x2: number, y2: number, text: string, fontSize: number, color: string) {
         const measurement = this.context.measureText(text);
         const textWidth = measurement.width;
@@ -138,4 +168,4 @@ export class SingleExplanatoryVariableCorrelationChart {
         this.context.fillText(text, 0, fontSize / 2);
         this.context.restore();
     }
-}
+  }
