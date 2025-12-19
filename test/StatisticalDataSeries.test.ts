395081cb55608c8c98739065662dd88c675ffa03
statsy <statsylib@gmail.com>
2025-12-18 20:03:31 -0500

    changing tests to use deno framework

--- test/StatisticalDataSeries.test.ts
+++ test/StatisticalDataSeries.test.ts
@@ -1,106 +1,106 @@
 import { StatisticalDataSeries } from '../src/StatisticalDataSeries.ts';
-import { expect, test } from '@jest/globals';
+import { assertEquals, assertAlmostEquals } from "https://deno.land/std@0.224.0/assert/mod.ts";
 
-test('count of [1,2,3] to be 3', () => {
-    expect(new StatisticalDataSeries([1,2,3]).count).toBe(3);
+Deno.test('count of [1,2,3] to be 3', () => {
+    assertEquals(new StatisticalDataSeries([1,2,3]).count, 3);
   });
 
-test('sum of [1,2,3] to be 6', () => {
-  expect(new StatisticalDataSeries([1,2,3], 'some name').sum).toBe(6);
+Deno.test('sum of [1,2,3] to be 6', () => {
+  assertEquals(new StatisticalDataSeries([1,2,3], 'some name').sum, 6);
 });
 
-test('mean of [1,2,3] to be 2', () => {
-expect(new StatisticalDataSeries([1,2,3]).mean).toBe(2);
+Deno.test('mean of [1,2,3] to be 2', () => {
+  assertEquals(new StatisticalDataSeries([1,2,3]).mean, 2);
 });
 
-test('count of [4,5,9] to be 3', () => {
-expect(new StatisticalDataSeries([4,5,9]).count).toBe(3);
+Deno.test('count of [4,5,9] to be 3', () => {
+  assertEquals(new StatisticalDataSeries([4,5,9]).count, 3);
 });
 
-test('min of [4,5,9] to be 4', () => {
-expect(new StatisticalDataSeries([4,5,9]).min).toBe(4);
+Deno.test('min of [4,5,9] to be 4', () => {
+  assertEquals(new StatisticalDataSeries([4,5,9]).min, 4);
 });
 
-test('max of [4,5,9] to be 9', () => {
-expect(new StatisticalDataSeries([4,5,9]).max).toBe(9);
+Deno.test('max of [4,5,9] to be 9', () => {
+  assertEquals(new StatisticalDataSeries([4,5,9]).max, 9);
 });
 
-test('sum of [4,5,9] to be 18', () => {
-  expect(new StatisticalDataSeries([4,5,9]).sum).toBe(18);
+Deno.test('sum of [4,5,9] to be 18', () => {
+  assertEquals(new StatisticalDataSeries([4,5,9]).sum, 18);
 });
 
-test('mean of [4,5,9] to be 6', () => {
-expect(new StatisticalDataSeries([4,5,9]).mean).toBe(6);
+Deno.test('mean of [4,5,9] to be 6', () => {
+  assertEquals(new StatisticalDataSeries([4,5,9]).mean, 6);
 });
 
-test('mode of [1,2,3] to be []', () => {
-expect(new StatisticalDataSeries([1,2,3]).mode).toEqual([]);
+Deno.test('mode of [1,2,3] to be []', () => {
+  assertEquals(new StatisticalDataSeries([1,2,3]).mode, []);
 });
 
-test('mode of [1,2,2,3] to be [2]', () => {
-expect(new StatisticalDataSeries([1,2,2,3]).mode).toEqual([2]);
+Deno.test('mode of [1,2,2,3] to be [2]', () => {
+  assertEquals(new StatisticalDataSeries([1,2,2,3]).mode, [2]);
 });
 
-test('mode of [1,2,2,3,3] to be [2,3]', () => {
-expect(new StatisticalDataSeries([1,2,2,3,3]).mode).toEqual([2,3]);
+Deno.test('mode of [1,2,2,3,3] to be [2,3]', () => {
+  assertEquals(new StatisticalDataSeries([1,2,2,3,3]).mode, [2,3]);
 });
 
-test('mode of [1,1,1,2,244,2,3,3,44,44,44,44,44,44,23,23,23,23] to be 44', () => {
-expect(new StatisticalDataSeries([1,1,1,2,2,2,3,3,44,44,44,44,44,44,23,23,23,23]).mode).toEqual([44]);
+Deno.test('mode of [1,1,1,2,244,2,3,3,44,44,44,44,44,44,23,23,23,23] to be 44', () => {
+  assertEquals(new StatisticalDataSeries([1,1,1,2,2,2,3,3,44,44,44,44,44,44,23,23,23,23]).mode, [44]);
 });
 
-test('median of [1,2,3] to be 2', () => {
-expect(new StatisticalDataSeries([1,2,3]).median).toBe(2);
+Deno.test('median of [1,2,3] to be 2', () => {
+  assertEquals(new StatisticalDataSeries([1,2,3]).median, 2);
 });
 
-test('median of [1,2,3,4] to be 2.5', () => {
-expect(new StatisticalDataSeries([1,2,3,4]).median).toEqual(2.5);
+Deno.test('median of [1,2,3,4] to be 2.5', () => {
+  assertEquals(new StatisticalDataSeries([1,2,3,4]).median, 2.5);
 });
 
-test('median of [342,532,6666,323,5541,45,1111] to be 532', () => {
-expect(new StatisticalDataSeries([342,532,6666,323,5541,45,1111]).median).toEqual(532);
+Deno.test('median of [342,532,6666,323,5541,45,1111] to be 532', () => {
+  assertEquals(new StatisticalDataSeries([342,532,6666,323,5541,45,1111]).median, 532);
 });
 
-test('median of [1,2,3] to be 2', () => {
-expect(new StatisticalDataSeries([1,2,3]).median).toEqual(2);
+Deno.test('median of [1,2,3] to be 2', () => {
+  assertEquals(new StatisticalDataSeries([1,2,3]).median, 2);
 });
 
-test('sumOfSquaredDeviationsFromMean of [44,55,6,666,111] to be 305289.2', () => {
-expect(new StatisticalDataSeries([44,55,6,666,111]).sumOfSquaredDeviationsFromMean).toEqual(305289.2);
+Deno.test('sumOfSquaredDeviationsFromMean of [44,55,6,666,111] to be 305289.2', () => {
+  assertEquals(new StatisticalDataSeries([44,55,6,666,111]).sumOfSquaredDeviationsFromMean, 305289.2);
 });
 
-test('sumOfSquaredDeviationsFromMean of [11,22,33,44,55] to be 1210', () => {
-expect(new StatisticalDataSeries([11,22,33,44,55]).sumOfSquaredDeviationsFromMean).toEqual(1210);
+Deno.test('sumOfSquaredDeviationsFromMean of [11,22,33,44,55] to be 1210', () => {
+  assertEquals(new StatisticalDataSeries([11,22,33,44,55]).sumOfSquaredDeviationsFromMean, 1210);
 });
 
-test('sample variance of [44,55,6,666,111] to be 76322.3', () => {
-expect(new StatisticalDataSeries([44,55,6,666,111]).sampleVariance).toEqual(76322.3);
+Deno.test('sample variance of [44,55,6,666,111] to be 76322.3', () => {
+  assertEquals(new StatisticalDataSeries([44,55,6,666,111]).sampleVariance, 76322.3);
 });
 
-test('sample variance of [11,22,33,44,55] to be 302.5', () => {
-expect(new StatisticalDataSeries([11,22,33,44,55]).sampleVariance).toEqual(302.5);
+Deno.test('sample variance of [11,22,33,44,55] to be 302.5', () => {
+  assertEquals(new StatisticalDataSeries([11,22,33,44,55]).sampleVariance, 302.5);
 });
 
-test('population variance of [44,55,6,666,111] to be 61057.84', () => {
-expect(new StatisticalDataSeries([44,55,6,666,111]).populationVariance).toBeCloseTo(61057.84);
+Deno.test('population variance of [44,55,6,666,111] to be 61057.84', () => {
+  assertAlmostEquals(new StatisticalDataSeries([44,55,6,666,111]).populationVariance, 61057.84);
 });
 
-test('population variance of [11,22,33,44,55] to be 242', () => {
-expect(new StatisticalDataSeries([11,22,33,44,55]).populationVariance).toEqual(242);
+Deno.test('population variance of [11,22,33,44,55] to be 242', () => {
+  assertEquals(new StatisticalDataSeries([11,22,33,44,55]).populationVariance, 242);
 });
 
-test('sample standard deviation of [44,55,6,666,111] to be 276.264909', () => {
-expect(new StatisticalDataSeries([44,55,6,666,111]).sampleStandardDeviation).toBeCloseTo(276.264909, 6);
+Deno.test('sample standard deviation of [44,55,6,666,111] to be 276.264909', () => {
+  assertAlmostEquals(new StatisticalDataSeries([44,55,6,666,111]).sampleStandardDeviation, 276.264909, 0.000001);
 });
 
-test('sample standard deviation of [11,22,33,44,55] to be 17.3925271', () => {
-expect(new StatisticalDataSeries([11,22,33,44,55]).sampleStandardDeviation).toBeCloseTo(17.3925271);
+Deno.test('sample standard deviation of [11,22,33,44,55] to be 17.3925271', () => {
+  assertAlmostEquals(new StatisticalDataSeries([11,22,33,44,55]).sampleStandardDeviation, 17.3925271);
 });
 
-test('population standard deviation of [44,55,6,666,111] to be 247.098847', () => {
-expect(new StatisticalDataSeries([44,55,6,666,111]).populationStandardDeviation).toBeCloseTo(247.098847, 6);
+Deno.test('population standard deviation of [44,55,6,666,111] to be 247.098847', () => {
+  assertAlmostEquals(new StatisticalDataSeries([44,55,6,666,111]).populationStandardDeviation, 247.098847, 0.000001);
 });
 
-test('population standard deviation of [11,22,33,44,55] to be 15.5563492', () => {
-expect(new StatisticalDataSeries([11,22,33,44,55]).populationStandardDeviation).toBeCloseTo(15.5563492, 6);
+Deno.test('population standard deviation of [11,22,33,44,55] to be 15.5563492', () => {
+  assertAlmostEquals(new StatisticalDataSeries([11,22,33,44,55]).populationStandardDeviation, 15.5563492, 0.000001);
 });
--- test/StatisticalDataSet.test.ts
+++ test/StatisticalDataSet.test.ts
@@ -1,237 +1,232 @@
 import { StatisticalDataSeries } from '../src/StatisticalDataSeries.ts';
 import { StatisticalDataSet } from '../src/StatisticalDataSet.ts';
-import { expect, test } from '@jest/globals';
+import { assertEquals, assertAlmostEquals, assertThrows } from "https://deno.land/std@0.224.0/assert/mod.ts";
 
-test('count of inner series elements val1', () => {
+Deno.test('count of inner series elements val1', () => {
     const data = [{"val1": 11,"val2": 22},{"val1": 11,"val2": 22}];
-    expect((new StatisticalDataSet(data).series('val1') as StatisticalDataSeries).count).toBe(2);
+    assertEquals((new StatisticalDataSet(data).series('val1') as StatisticalDataSeries).count, 2);
 });
 
-test('count of inner series elements val2', () => {
+Deno.test('count of inner series elements val2', () => {
     const data = [{"val1": 11,"val2": 22},{"val1": 11,"val2": 22}];
     const series = new StatisticalDataSet(data).series('val2') as StatisticalDataSeries;
-    expect(series.count).toBe(2);
+    assertEquals(series.count, 2);
 });
 
-test('name of inner series val2', () => {
+Deno.test('name of inner series val2', () => {
     const data = [{"val1": 11,"val2": 22},{"val1": 11,"val2": 33}];
-    expect((new StatisticalDataSet(data).series('val2') as StatisticalDataSeries).name).toBe('val2');
+    assertEquals((new StatisticalDataSet(data).series('val2') as StatisticalDataSeries).name, 'val2');
 });
 
-test('sum of inner series elements val2', () => {
+Deno.test('sum of inner series elements val2', () => {
     const data = [{"val1": 11,"val2": 22},{"val1": 11,"val2": 33}];
-    expect((new StatisticalDataSet(data).series('val2') as StatisticalDataSeries).sum).toBe(55);
+    assertEquals((new StatisticalDataSet(data).series('val2') as StatisticalDataSeries).sum, 55);
 });
 
-test('mean of inner series elements val2', () => {
+Deno.test('mean of inner series elements val2', () => {
     const data = [{"val1": 11,"val2": 40},{"val1": 11,"val2": 20}];
-    expect((new StatisticalDataSet(data).series('val2') as StatisticalDataSeries).mean).toBe(30);
+    assertEquals((new StatisticalDataSet(data).series('val2') as StatisticalDataSeries).mean, 30);
 });
 
-test('increase data point simple case', () => {
+Deno.test('increase data point simple case', () => {
     const data = [{"val1": 11,"val2": 40},{"val1": 11,"val2": 20}];
     const dataset = new StatisticalDataSet(data);
     dataset.increaseDataPoint(0, 'val2', 3)
-    expect((dataset.data as any)[0]['val2']).toBe(43);
+    assertEquals((dataset.data as any)[0]['val2'], 43);
 });
 
-test('decrease data point simple case', () => {
+Deno.test('decrease data point simple case', () => {
     const data = [{"val1": 11,"val2": 40},{"val1": 11,"val2": 20}];
     const dataset = new StatisticalDataSet(data);
     dataset.decreaseDataPoint(0, 'val1', 2)
-    expect((dataset.data[0] as any)['val1']).toBe(9);
+    assertEquals((dataset.data[0] as any)['val1'], 9);
 });
 
-test('decrease data point negative case', () => {
+Deno.test('decrease data point negative case', () => {
     const data = [{"val1": 11,"val2": 40},{"val1": 11,"val2": 20}];
     const dataset = new StatisticalDataSet(data);
     dataset.decreaseDataPoint(1, 'val2', 34)
-    expect((dataset.data[1] as any)['val2']).toBe(-14);
+    assertEquals((dataset.data[1] as any)['val2'], -14);
 });
 
-test('maxKeyValue simple case', () => {
+Deno.test('maxKeyValue simple case', () => {
     const data = [{"val1": 5,"val2": 40},{"val1": 11,"val2": 20}];
     const dataset = new StatisticalDataSet(data);
-    expect(dataset.maxKeyValue('val1')).toBe(11);
+    assertEquals(dataset.maxKeyValue('val1'), 11);
 });
 
-test('maxKeyValue duplicate case', () => {
+Deno.test('maxKeyValue duplicate case', () => {
     const data = [{"val1": 11,"val2": 40},{"val1": 34,"val2": 40}];
     const dataset = new StatisticalDataSet(data);
-    expect(dataset.maxKeyValue('val2')).toBe(40);
+    assertEquals(dataset.maxKeyValue('val2'), 40);
 });
 
-test('maxKeyValue other simple case', () => {
+Deno.test('maxKeyValue other simple case', () => {
     const data = [{"val1": 11,"val2": 50},{"val1": 34,"val2": 40}];
     const dataset = new StatisticalDataSet(data);
-    expect(dataset.maxKeyValue('val2')).toBe(50);
+    assertEquals(dataset.maxKeyValue('val2'), 50);
 });
 
-test('minKeyValue simple case', () => {
+Deno.test('minKeyValue simple case', () => {
     const data = [{"val1": 5,"val2": 40},{"val1": 11,"val2": 20}];
     const dataset = new StatisticalDataSet(data);
-    expect(dataset.minKeyValue('val1')).toBe(5);
+    assertEquals(dataset.minKeyValue('val1'), 5);
 });
 
-test('minKeyValue duplicate case', () => {
+Deno.test('minKeyValue duplicate case', () => {
     const data = [{"val1": 11,"val2": 40},{"val1": 34,"val2": 40}];
     const dataset = new StatisticalDataSet(data);
-    expect(dataset.minKeyValue('val2')).toBe(40);
+    assertEquals(dataset.minKeyValue('val2'), 40);
 });
 
-test('minKeyValue other simple case', () => {
+Deno.test('minKeyValue other simple case', () => {
     const data = [{"val1": 11,"val2": 50},{"val1": 34,"val2": 40}];
     const dataset = new StatisticalDataSet(data);
-    expect(dataset.minKeyValue('val2')).toBe(40);
+    assertEquals(dataset.minKeyValue('val2'), 40);
 });
 
-test('leastSquaresRegression simple positive slope', () => {
+Deno.test('leastSquaresRegression simple positive slope', () => {
     const data = [{"val1": 5,"val2": 1},{"val1": 10,"val2": 3},{"val1": 15,"val2": 5}];
     const dataset = new StatisticalDataSet(data);
-    expect(dataset.leastSquaresRegression('val1', 'val2').m).toBe(0.4);
-    expect(dataset.leastSquaresRegression('val1', 'val2').b).toBe(-1);
+    assertEquals(dataset.leastSquaresRegression('val1', 'val2').m, 0.4);
+    assertEquals(dataset.leastSquaresRegression('val1', 'val2').b, -1);
 });
 
-test('leastSquaresRegression not so simple negative slope', () => {
+Deno.test('leastSquaresRegression not so simple negative slope', () => {
     const data = [{"val1": 3,"val2": 7},{"val1": 6,"val2": 8},{"val1": 9,"val2": 9},{"val1": 12,"val2": 9},{"val1": 15,"val2": 11}];
     const dataset = new StatisticalDataSet(data);
-    expect(dataset.leastSquaresRegression('val1', 'val2').m).toBeCloseTo(0.3);
-    expect(dataset.leastSquaresRegression('val1', 'val2').b).toBeCloseTo(6.1);
+    assertAlmostEquals(dataset.leastSquaresRegression('val1', 'val2').m, 0.3);
+    assertAlmostEquals(dataset.leastSquaresRegression('val1', 'val2').b, 6.1);
 });
 
-test('correlationCoefficient simple case', () => {
+Deno.test('correlationCoefficient simple case', () => {
     const data = [{"val1": 3,"val2": 7},{"val1": 6,"val2": 8},{"val1": 9,"val2": 9},{"val1": 12,"val2": 9},{"val1": 15,"val2": 11}];
     const dataset = new StatisticalDataSet(data);
-    expect(dataset.correlationCoefficient('val1', 'val2')).toBeCloseTo(0.95940, 5);
+    assertAlmostEquals(dataset.correlationCoefficient('val1', 'val2'), 0.95940, 5);
 });
 
-test('correlationCoefficient perfect case', () => {
+Deno.test('correlationCoefficient perfect case', () => {
     const data = [{"val1": 3,"val2": 3},{"val1": 6,"val2": 6},{"val1": 9,"val2": 9},{"val1": 12,"val2": 12},{"val1": 15,"val2": 15}];
     const dataset = new StatisticalDataSet(data);
-    expect(dataset.correlationCoefficient('val1', 'val2')).toBeCloseTo(1, 5);
+    assertAlmostEquals(dataset.correlationCoefficient('val1', 'val2'), 1);
 });
 
-test('correlationCoefficient low case', () => {
+Deno.test('correlationCoefficient low case', () => {
     const data = [{"val1": 3,"val2": 5},{"val1": 6,"val2": 9},{"val1": 9,"val2": 1},{"val1": 12,"val2": 10},{"val1": 15,"val2": 5}];
     const dataset = new StatisticalDataSet(data);
-    expect(dataset.correlationCoefficient('val1', 'val2')).toBeCloseTo(0.04385, 5);
+    assertAlmostEquals(dataset.correlationCoefficient('val1', 'val2'), 0.04385, 0.00001);
 });
 
-test('correlationCoefficient negarive case', () => {
+Deno.test('correlationCoefficient negarive case', () => {
     const data = [{"val1": 3,"val2": 9},{"val1": 6,"val2": 7},{"val1": 9,"val2": 5},{"val1": 12,"val2": 3},{"val1": 15,"val2": 1}];
     const dataset = new StatisticalDataSet(data);
-    expect(dataset.correlationCoefficient('val1', 'val2')).toBeCloseTo(-1, 5);
+    assertAlmostEquals(dataset.correlationCoefficient('val1', 'val2'), -1, 0.00001);
 });
 
-test('numerical series 0', () => {
+Deno.test('numerical series 0', () => {
     const dataset = new StatisticalDataSet([{"val1": "r","val2": "t"},{"val1": "r","val2": "e"}]);
-    expect(dataset.numericalSeries().length).toBe(0);
+    assertEquals(dataset.numericalSeries().length, 0);
 });
 
-test('numerical series 1', () => {
+Deno.test('numerical series 1', () => {
     const dataset = new StatisticalDataSet([{"val1": "val1_1","val2": 9},{"val1": "val1_2","val2": 7}]);
-    expect(dataset.numericalSeries().length).toBe(1);
+    assertEquals(dataset.numericalSeries().length, 1);
 });
 
-test('numerical series 2', () => {
+Deno.test('numerical series 2', () => {
     const dataset = new StatisticalDataSet([{"val1": "1","val2": 9},{"val1": "2","val2": 7}]);
-    expect(dataset.numericalSeries().length).toBe(2);
+    assertEquals(dataset.numericalSeries().length, 2);
 });
 
-test('numerical series undefined', () => {
+Deno.test('numerical series undefined', () => {
     const dataset = new StatisticalDataSet([{"val1": "1","val2": 9},{"val3": "a","val4": "f"}]);
-    expect(dataset.isUniform).toBe(false);
-    expect(dataset.numericalSeries().length).toBe(0);
+    assertEquals(dataset.isUniform, false);
+    assertEquals(dataset.numericalSeries().length, 0);
 });
 
-test('non uniform series 1', () => {
+Deno.test('non uniform series 1', () => {
     const dataset = new StatisticalDataSet([{"val1": "1","val2": 9},{"val3": "a","val4": "f"}]);
-    expect(dataset.isUniform).toBe(false);
+    assertEquals(dataset.isUniform, false);
 });
 
-test('uniform series 2', () => {
+Deno.test('uniform series 2', () => {
     const dataset = new StatisticalDataSet([{"val1": "1","val2": 9},{"val1": "a","val2": "f"}]);
-    expect(dataset.isUniform).toBe(true);
+    assertEquals(dataset.isUniform, true);
 });
 
-test('load hints nonsense with 2 nonsense series', () => {
+Deno.test('load hints nonsense with 2 nonsense series', () => {
     const dataset = new StatisticalDataSet({"nonsense key": "nonsense value", "data": [{"val1": "1","val2": 9},{"val1": "a","val2": "f"}]});
-    expect(dataset.isUniform).toBe(true);
+    assertEquals(dataset.isUniform, true);
 
 });
 
-test('load hints nonsense with 2 string series', () => {
+Deno.test('load hints nonsense with 2 string series', () => {
     const dataset = new StatisticalDataSet({"nonsense key": "nonsense value", "data": [{"val1": "r","val2": "g"},{"val1": "a","val2": "f"}]});
-    expect(dataset.isUniform).toBe(true);
+    assertEquals(dataset.isUniform, true);
 
 });
 
-test('load hints nonsense with 2 numerical series', () => {
+Deno.test('load hints nonsense with 2 numerical series', () => {
     const dataset = new StatisticalDataSet({"nonsense key": "nonsense value", "data": [{"val1": "1","val2": "9"},{"val1": "4","val2": "3"}]});
-    expect(dataset.isUniform).toBe(true);
-    expect(dataset.numericalSeries().length).toBe(2);
+    assertEquals(dataset.isUniform, true);
+    assertEquals(dataset.numericalSeries().length, 2);
 });
 
-test('load hints nonsense with 1 numerical series and 1 string series', () => {
+Deno.test('load hints nonsense with 1 numerical series and 1 string series', () => {
     const dataset = new StatisticalDataSet({"nonsense key": "nonsense value", "data": [{"val1": 1,"val2": "a"},{"val1": 2,"val2": "b"}]});
-    expect(dataset.isUniform).toBe(true);
-    expect(dataset.numericalSeries().length).toBe(1);
+    assertEquals(dataset.isUniform, true);
+    assertEquals(dataset.numericalSeries().length, 1);
 
 });
 
-test('load hints nonsense with 1 numerical series and 1 string series', () => {
+Deno.test('load hints nonsense with 1 numerical series and 1 string series', () => {
     const dataset = new StatisticalDataSet({"nonsense key": "nonsense value", "data": [{"val1": 1,"val2": "a"},{"val1": 2,"val2": "b"}]});
-    expect(dataset.isUniform).toBe(true);
-    expect(dataset.numericalSeries().length).toBe(1);
-
+    assertEquals(dataset.isUniform, true);
+    assertEquals(dataset.numericalSeries().length, 1);
 });
 
-test('load hints metadata with dependent variable name happy path', () => {
+Deno.test('load hints metadata with dependent variable name happy path', () => {
     const dataset = new StatisticalDataSet({"dependent_variable_name": "val1", "data": [{"val1": 1,"val2": 2},{"val1": 3,"val2": 4}]});
-    expect(dataset.isUniform).toBe(true);
-    expect(dataset.numericalSeries().length).toBe(2);
-
+    assertEquals(dataset.isUniform, true);
+    assertEquals(dataset.numericalSeries().length, 2);
 });
 
-test('load hints metadata with dependent variable name and default label happy path', () => {
+Deno.test('load hints metadata with dependent variable name and default label happy path', () => {
     const dataset = new StatisticalDataSet({"dependent_variable_name": "val1", "data": [{"val1": 1,"val2": 2, "label": "p1"},{"val1": 3,"val2": 4, "label": "p2"}]});
-    expect(dataset.isUniform).toBe(true);
-    expect(dataset.numericalSeries().length).toBe(2);
-
+    assertEquals(dataset.isUniform, true);
+    assertEquals(dataset.numericalSeries().length, 2);
 });
 
-test('load hints metadata with dependent variable name and non default label happy path', () => {
+Deno.test('load hints metadata with dependent variable name and non default label happy path', () => {
     const dataset = new StatisticalDataSet({"dependent_variable_name": "val1", "label_name": "notlabel", "data": [{"val1": 1,"val2": 2, "notlabel": "p1"},{"val1": 3,"val2": 4, "notlabel": "p2"}]});
-    expect(dataset.isUniform).toBe(true);
-    expect(dataset.numericalSeries().length).toBe(2);
-
+    assertEquals(dataset.isUniform, true);
+    assertEquals(dataset.numericalSeries().length, 2);
 });
 
-test('default labels because it is the only string series', () => {
+Deno.test('default labels because it is the only string series', () => {
     const dataset = new StatisticalDataSet([{"val1": 1,"val2": 2, "notlabelbutreallyis": "thelabel2"},{"val1": 3,"val2": 4, "notlabelbutreallyis": "thelabel2"}]);
-
 });
 
-test('should throw an error if called without an arg', () => {
-    expect(() => {
+Deno.test('should throw an error if called without an arg', () => {
+    assertThrows(() => {
         new StatisticalDataSet([{"val1": "1","val2": 9},{"val3": "a","val4": "f"}]).correlationCoefficient('val1', 'val2');
-    }).toThrow('Dataset is not uniform.  Unable to calculate coefficient.');
+    }, 'Dataset is not uniform.  Unable to calculate coefficient.');
 })
 
-  test('should throw an error if called without an arg', () => {
-    expect(() => {
+Deno.test('should throw an error if called without an arg', () => {
+    assertThrows(() => {
         new StatisticalDataSet([{"val1": "1","val2": 9},{"val1": "a","val2": "f"}]).correlationCoefficient('val1', 'val2');
-    }).toThrow('Invalid data: val1 and val2 values must be numbers.');
+    }, 'Invalid data: val1 and val2 values must be numbers.');
 })
 
-  test('should throw an error if called without an arg', () => {
-    expect(() => {
+Deno.test('should throw an error if called without an arg', () => {
+    assertThrows(() => {
         new StatisticalDataSet([]).correlationCoefficient('val1', 'val2');
-    }).toThrow('Data must be an array with at least two elements.');
+    }, 'Data must be an array with at least two elements.');
 })
 
-  test('should throw an error if called without an arg', () => {
-    expect(() => {
+Deno.test('should throw an error if called without an arg', () => {
+    assertThrows(() => {
         new StatisticalDataSet([{"val1": 0,"val2": 0},{"val1": 0,"val2": 0}]).correlationCoefficient('val1', 'val2');
-    }).toThrow('Denominator is zero, correlation cannot be calculated.');
+    }, 'Denominator is zero, correlation cannot be calculated.');
 })
